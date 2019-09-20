0fcd7239afcf9df39204c9f979cef64a39c666fa
silensmile <silensmile@outlook.com>
2019-09-20 11:10:01 +0800

    922

---
+++ Practice/排序按奇偶排序数组2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<map>
+
+using namespace std;
+
+vector<int> sortArrayByParityII(vector<int>& A) {
+    vector<int> ans(A.size());
+    int odd = 0,even = 0;
+    map<int,int> cnt;
+    for (auto i : A)
+        cnt[i]++;
+    for (auto i : cnt) {
+        for (int j = 0; j < i.second; j++) {
+            if (i.first % 2) {
+                ans[odd] = i.first;
+                odd += 2;
+            }
+            else {
+                ans[even] = i.first;
+                even += 2;
+            }
+        }
+    }
+    return ans;
+}
